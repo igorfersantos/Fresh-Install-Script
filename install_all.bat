849409d81ba2e621fce5a52f282e416a32b2104e
Igor Fernandes <snkcoins@gmail.com>
2021-09-27 20:33:02 -0300

    Update install_all.bat

--- install_all.bat
+++ install_all.bat
@@ -6,7 +6,7 @@ powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-cinst -y python
+choco install -y python
 
 python -m pip install --upgrade pip
 pip install tqdm
@@ -14,4 +14,21 @@ pip install requests
 
 python downloadPrograms.py
 
-cinst -y discord imageglass malwarebytes pycharm-community twitch msiafterburner revo-uninstaller obs-studio playnite steam origin intellijidea-community visualstudio2019community epicgameslauncher vscode firefox googlechrome jdk8 jre8 7zip notepadplusplus github-desktop winrar postman vlc k-litecodecpackfull thunderbird openjdk
+REM ----ESSENTIAL-----
+choco install -y vcredist-all
+
+REM ----CHAT----
+choco install -y discord 
+
+REM ----UTILS----
+choco install -y imageglass msiafterburner 7zip notepadplusplus winrar thunderbird revo-uninstaller obs-studio vlc k-litecodecpackfull
+
+REM ----GAMES RELATED----
+choco install -y twitch steam origin epicgameslauncher
+
+REM ----INTERNET----
+choco install -y firefox microsoft-edge googlechrome
+
+REM ----DEV----
+choco install -y pycharm-community intellijidea-community visualstudio2019community vscode jdk8 jre8 github-desktop postman openjdk
+malwarebytes 
