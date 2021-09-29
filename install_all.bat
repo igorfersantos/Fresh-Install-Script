547659f61677b7ea1d7fcd62725b3970ef06d621
Igor Fernandes <snkcoins@gmail.com>
2021-09-29 01:20:13 -0300

    Update install_all.bat

--- install_all.bat
+++ install_all.bat
@@ -6,8 +6,10 @@ powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+start "Installing VC++ Runtimes All-in-One" cmd.exe @cmd /k "curl -o ./vc-all/vc-all.zip https://us6-dl.techpowerup.com/files/GgKLcA0NM_MtKeunWPdLoQ/1632925613/Visual-C-Runtimes-All-in-One-Jul-2021.zip --create-dirs & tar -xvf ./vc-all/vc-all.zip -C ./vc-all & del .\vc-all\*.zip & call ./vc-all/install_all.bat & pause"
+
 REM ----ESSENTIAL-----
-choco install -y vcredist-all nvidia-display-driver geforce-game-ready-driver
+choco install -y  nvidia-display-driver geforce-game-ready-driver
 
 REM ----CHAT----
 choco install -y discord 
